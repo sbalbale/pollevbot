4918ec5ad1b634f19bcfedbc13f663259979f0a6
Sean Balbale <sean.balbale@gmail.com>
2023-09-19 12:40:50 -0400

    .env for security

--- pollevbot/main.py
+++ pollevbot/main.py
@@ -1,10 +1,18 @@
 from pollbot import PollBot
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
 
 
 def main():
-    user = 'balbale.s'
-    password = '41HillcrestRoad!'
-    host = 'vasilikilyko643'
+    usrName = os.getenv('USER')
+    passwrd = os.getenv('PASSWORD')
+    pollHost = os.getenv('HOST')
+    user = usrName
+    password = passwrd
+    host = pollHost
 
     # If you're using a non-neu PollEv account,
     # add the argument "login_type='pollev'"
